845f97138bbd4d37165d76842688c833d992eb30
Joshua Cooper <joshua@cooper.cx>
2019-12-26 15:17:21 +0900

    Added examples doc comment for crates.io.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+/*!
+# supermemo2
+
+This crate implements the core components of the supermemo2 spaced repetition algorithm.
+
+# Examples
+
+```rust
+use supermemo2::Item;
+
+pub fn main() {
+    let item = Item::default();
+    let interval = item
+        .review(4)
+        .unwrap()
+        .review(3)
+        .unwrap()
+        .review(5)
+        .unwrap()
+        .interval();
+
+    assert_eq!(interval, 15);
+}
+```
+*/
+
 use std::default::Default;
 use std::error::Error as StdError;
 use std::fmt;
